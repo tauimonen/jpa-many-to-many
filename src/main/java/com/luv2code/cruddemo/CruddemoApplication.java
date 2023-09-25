bfe41c0696af893eb9ba613871e2ded4feb27c67
vgtoui <toni.uimonen@tuni.fi>
2023-09-25 13:59:43 +0300

    Create findInstructorById functionality

--- src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -19,18 +19,42 @@ public class CruddemoApplication {
 	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 
 		return runner -> {
-			createInstructor(appDAO);
+			// createInstructor(appDAO);
+
+			findInstructor(appDAO);
 		};
+	}
+
+	private void findInstructor(AppDAO appDAO) {
+
+		int theId = 2;
+		System.out.println("Finding instructor is: " + theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		System.out.println("tempInstructor: " + tempInstructor);
+		System.out.println("the associated instructorDetail only: "
+				+ tempInstructor.getInstructorDetail());
+
 	}
 
 	private void createInstructor(AppDAO appDAO) {
 
-		Instructor tempInstructor = new Instructor("Chad",
+		// create some instructors and details
+
+/*		Instructor tempInstructor = new Instructor("Chad",
 				"Darby", "darby@luv2code.com");
 
 		InstructorDetail tempInstructorDetail = new InstructorDetail(
 				"http://www.luv2code.com/youtube",
-				"Luv 2 code !!!");
+				"Luv 2 code !!!");*/
+
+		Instructor tempInstructor = new Instructor("Madhu",
+				"Patel", "madhu@luv2code.com");
+
+		InstructorDetail tempInstructorDetail = new InstructorDetail(
+				"http://www.luv2code.com/youtube",
+				"guitar !!!");
 
 		// associate the objects
 		tempInstructor.setInstructorDetail(tempInstructorDetail);
