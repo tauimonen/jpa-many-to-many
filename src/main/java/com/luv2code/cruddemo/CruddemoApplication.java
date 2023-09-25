7d57e335971ec5e542d58a8c2d27767b4d064ca2
vgtoui <toni.uimonen@tuni.fi>
2023-09-25 14:36:13 +0300

    first commit

--- src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -21,8 +21,19 @@ public class CruddemoApplication {
 		return runner -> {
 			// createInstructor(appDAO);
 
-			findInstructor(appDAO);
+			// findInstructor(appDAO);
+
+			deleteInstructor(appDAO);
 		};
+	}
+
+	private void deleteInstructor(AppDAO appDAO) {
+
+		int theId = 1;
+		System.out.println("Deleting instructor is: " + theId);
+
+		appDAO.deleteInstructorById(theId);
+		System.out.println("Done!");
 	}
 
 	private void findInstructor(AppDAO appDAO) {
